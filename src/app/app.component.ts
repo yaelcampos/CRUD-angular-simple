94c21ff382a8820996373caba66733a3eaad54df
yael campos <yaelmagdalenacampos@gmail.com>
2022-02-11 13:37:09 -0300

    Finalizamos la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { Employee } from './models/employee';
 
@@ -13,4 +14,30 @@ export class AppComponent {
     {id: 2, name:"Mathew", country: "USA"},
     {id: 3, name:"Ada", country: "USA"},
   ];
+
+
+selectedEmployee: Employee = new Employee();
+
+openForEdit(employee: Employee) {
+  this.selectedEmployee = employee;
+}
+
+addOrEdit() {
+  if(this.selectedEmployee.id === 0){
+    this.selectedEmployee.id = this.employeeArray.length + 1;
+    this.employeeArray.push(this.selectedEmployee);
+
+  }
+  this.selectedEmployee = new Employee();
+}
+
+delete(){
+ //si presiona ok, me da TRUE y ejecuta 
+  if (confirm('¿Estas seguro que quieres eliminar el empleado?')){ 
+     //quita el empleado
+  this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+  this.selectedEmployee = new Employee();
+  }
+}
+
 }
